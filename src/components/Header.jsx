cdb590bc991197c3d5e13aff8406b9f043f3b957
ahmetberktas <ahmetberktas50@gmail.com>
2024-09-18 19:19:52 +0300

    Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+const Header = ({ title }) => {
+  const [activeButton, setActiveButton] = useState("sana-ozel");
+  return (
+    <header className="sticky top-0 z-10 bg-black/[0.65] backdrop-blur-md">
+      <h3 className="px-4 h-[53px] flex items-center text-xl font-bold">
+        {title}
+      </h3>
+      <div className="flex">
+        <button
+          type="button"
+          className="flex-auto text-center"
+          onClick={() => setActiveButton("sana-ozel")}
+        >
+          <div className="h-[53px] relative inline-flex items-center">
+            Sana özel
+            {activeButton === "sana-ozel" && (
+              <div className="h-1 absolute bottom-0 left-0 w-full rounded-full bg-[#1D9BF0]"></div>
+            )}
+          </div>
+        </button>
+        <button
+          type="button"
+          className="flex-auto text-center"
+          onClick={() => setActiveButton("takip-edilenler")}
+        >
+          <div className="h-[53px] relative inline-flex items-center">
+            Takip edilenler
+            {activeButton === "takip-edilenler" && (
+              <div className="h-1 absolute bottom-0 left-0 w-full rounded-full bg-[#1D9BF0]"></div>
+            )}
+          </div>
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
 import RightBar from "../components/RightBar";
+import Header from "../components/Header";
 
 const MainPage = () => {
   return (
@@ -8,7 +9,7 @@ const MainPage = () => {
       <Sidebar></Sidebar>
       <div className="flex-1 flex gap-[30px]">
         <main className="flex-1 max-w-[600px] border-x border-[#2f3336]">
-          Main Page
+          <Header title="Anasayfa"></Header>
         </main>
         <RightBar></RightBar>
       </div>
